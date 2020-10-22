186759eed28072b860d4d062d24864c230caba0a
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-22 15:07:30 +0200

    return what I learned about Windows

---
+++ windows_test/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# https://github.com/jkramarz/recovery/blob/master/run.sh
+# https://docs.ansible.com/ansible/latest/modules/apt_module.html
+# https://docs.ansible.com/ansible/latest/user_guide/playbooks_reuse_roles.html
+
+sudo apt-get install ansible -y
+echo "localhost ansible_connection=local user=$USER" > inventory
+ansible-playbook -v -i inventory site.yml
